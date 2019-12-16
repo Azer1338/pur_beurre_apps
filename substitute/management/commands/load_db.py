9ee9a03114bdf9de0ff3af9354b5d61420e1d80b
Azer1338 <adrien13.f@gmail.com>
2019-12-16 21:27:06 +0100

    custom manage.py function added.

---
+++ substitute/management/commands/load_db.py
@@ -0,0 +1,20 @@
+from django.core.management import BaseCommand
+from substitute.data_base_handler import DataBaseTableHandler
+from substitute.models import Aliment
+from substitute.open_food_facts_handler import OpenFoodFactsAPIHandler
+
+
+class Command(BaseCommand):
+    help = 'Load the DB from API'
+
+    def handle(self, *args, **kwargs):
+        # Gather data from API
+        api_handler = OpenFoodFactsAPIHandler()
+        api_handler.generate_substitutes_dict()
+
+        # Load data in the database
+        aliment_table_handler = DataBaseTableHandler(Aliment)
+        aliment_table_handler.load_json_file_in_table(api_handler.substitutes_list)
+
+        print(aliment_table_handler.message)
+
---
+++ substitute/management/commands/unload_db.py
@@ -0,0 +1,18 @@
+from django.core.management import BaseCommand
+from substitute.data_base_handler import DataBaseTableHandler
+from substitute.models import Aliment, UserLinkToAlimentsTable
+
+
+class Command(BaseCommand):
+    help = 'Load the DB from API'
+
+    def handle(self, *args, **kwargs):
+        # Remove data from table
+        aliment_table_handler = DataBaseTableHandler(Aliment)
+        aliment_table_handler.clear_table()
+        favorite_table_handler = DataBaseTableHandler(UserLinkToAlimentsTable)
+        favorite_table_handler.clear_table()
+
+        # Console display
+        print(aliment_table_handler.message)
+        print(favorite_table_handler.message)

2323a397bc06ad098d9dcbf345e571e1689b9c00
Azer1338 <adrien13.f@gmail.com>
2020-01-05 16:19:37 +0100

    Pur_beurre_project changes

--- pur_beurre_project/urls.py
+++ pur_beurre_project/urls.py
@@ -1,4 +1,4 @@
-"""Pur_beurre_apps URL Configuration
+"""Pur_beurre_project URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.2/topics/http/urls/
--- pur_beurre_project/wsgi.py
+++ pur_beurre_project/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for Pur_beurre_apps project.
+WSGI config for Pur_beurre_project project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Pur_beurre_apps.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Pur_beurre_project.settings')
 
 application = get_wsgi_application()
